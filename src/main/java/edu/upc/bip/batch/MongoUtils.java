c8ee18ea70f8f71041c579fd922ca601cc983d8c
katsiaryna <katherine.krasnoschok@hotmail.com>
2016-12-19 03:20:40 +0100

    trajectories fix

--- src/main/java/edu/upc/bip/batch/MongoUtils.java
+++ src/main/java/edu/upc/bip/batch/MongoUtils.java
@@ -86,7 +86,7 @@ public class MongoUtils {
 
                 //System.out.println(cursor.next());
                 Document tobj = (Document) cursor.next();
-                System.out.println(tobj.get(field).toString());
+                //System.out.println(tobj.get(field).toString());
                 result.add((field == null) ? tobj.toJson() : tobj.get(field).toString());
             }
         } finally {
